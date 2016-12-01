920854e31118c1f14d89a9c84cc6c33e2da6adbd
shrumit <shrumit.m@hotmail.com>
2016-11-30 22:11:11 -0500

    Develop create, update, delete for Movie

--- __init__.py
+++ __init__.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, session, g
-# import db_login
+import mysql.connector
 
 app = Flask(__name__)
 app.debug = True
+
+config = {
+  'user': 'root',
+  'password': 'station',
+#  'host': '127.0.0.1',
+  'database': 'MovieTheatre'
+}
 
 @app.route('/')
 def index():
@@ -21,7 +28,8 @@ def login():
 		session['username'] = request.form['username']
 		return redirect(url_for('profile'))
 	else: # user not found
-		return redirect(url_for('index'))
+		error = "No such user. Please try again."
+		return render_template('index.html', error=error, username=request.form['username'])
 
 def findCustomer(user):
 	if user == 'john': return True
@@ -42,10 +50,12 @@ def logout():
 # Customer Pages
 @app.route('/customer')
 def customer():
+	# if 'username' not in session: return redirect(url_for('logout'))
 	return redirect(url_for('profile'))
 
 @app.route('/customer/profile')
 def profile():
+	if not 'username' in session: return redirect(url_for('logout'))
 	return render_template('customer/profile.html')
 
 @app.route('/customer/search')
@@ -62,9 +72,67 @@ def ratings():
 def backend():
 	return redirect(url_for('movies'))
 
+# MOVIES
 @app.route('/backend/movies')
 def movies():
-	return render_template('backend/movies.html')
+	cnx = mysql.connector.connect(**config)
+	cursor = cnx.cursor()
+	query = ("select * from Movie")
+	cursor.execute(query)
+	result = cursor.fetchall()
+	cnx.close()
+	return render_template('backend/movies.html', data=result)
+
+@app.route('/backend/movies/add', methods=['POST'])
+def movies_add():
+	cnx = mysql.connector.connect(**config)
+	cursor = cnx.cursor()
+	query = ("insert into Movie(MovieName, MovieYear) values (%s, %s)")
+	data = (request.form['name'], request.form['year'])
+	if data[0] == "":
+		session['movie_message'] = "Add Movie Unsuccessful: Non-null field cannot be empty."
+		return redirect(url_for('movies'))
+	try:
+		cursor.execute(query, data)
+		cnx.commit()
+		cnx.close()
+		session['movie_message'] = "Add Movie Successful: %s, %s" % (data)
+	except mysql.connector.Error as err:
+		session['movie_message'] = "Add Movie Unsuccessful: %s" % err.msg
+
+	return redirect(url_for('movies'))
+	
+@app.route('/backend/movies/delete', methods=['POST'])
+def movies_delete():
+	cnx = mysql.connector.connect(**config)
+	cursor = cnx.cursor()
+	query = ("delete from Movie where idMovie=%s")
+	data = (request.form['submit'],)
+	try:
+		cursor.execute(query, data)
+		cnx.commit()
+		cnx.close()
+		session['movie_message'] = "Delete Movie Successful"
+	except mysql.connector.Error as err:
+		session['movie_message'] = "Delete Movie Unsuccessful: %s" % err.msg
+	
+	return redirect(url_for('movies'))
+
+@app.route('/backend/movies/modify', methods=['POST'])
+def movies_modify():
+	cnx = mysql.connector.connect(**config)
+	cursor = cnx.cursor()
+	query = ("update Movie set MovieName=%s, MovieYear=%s where idMovie=%s")
+	data = (request.form['name'],request.form['year'],request.form['id'])
+	try:
+		cursor.execute(query, data)
+		cnx.commit()
+		cnx.close()
+		session['movie_message'] = "Modify Movie Successful"
+	except mysql.connector.Error as err:
+		session['movie_message'] = "Modify Movie Unsuccessful: %s" % err.msg
+	
+	return redirect(url_for('movies'))
 
 @app.route('/backend/rooms')
 def rooms():
