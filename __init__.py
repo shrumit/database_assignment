1fa5ef365fdc56daa9304ea173c8ad5a2e6fa1c8
shrumit <shrumit.m@hotmail.com>
2016-12-02 17:18:12 -0500

    Add Customer functionalities

--- __init__.py
+++ __init__.py
@@ -32,8 +32,18 @@ def login():
 		return render_template('index.html', error=error, username=request.form['username'])
 
 def findCustomer(user):
-	if user == 'john': return True
-	else: return False
+	cnx = mysql.connector.connect(**config)
+	cursor = cnx.cursor()
+	print "user", user
+	query = ("select * from Customer where EmailAddress=%s")
+	data = (user,)
+	cursor.execute(query, data)
+	result = cursor.fetchall()
+	cnx.close()
+	if len(result) == 1:
+		return True
+	else:
+		return False
 
 @app.route('/login_backend', methods=['POST'])
 def login_backend():
@@ -50,22 +60,118 @@ def logout():
 # Customer Pages
 @app.route('/customer')
 def customer():
-	# if 'username' not in session: return redirect(url_for('logout'))
 	return redirect(url_for('profile'))
 
 @app.route('/customer/profile')
 def profile():
-	if not 'username' in session: return redirect(url_for('logout'))
-	return render_template('customer/profile.html')
+	cnx = mysql.connector.connect(**config)
+	cursor = cnx.cursor()
+	query = ("select * from Customer where EmailAddress=%s")
+	data = (session['username'],)
+	cursor.execute(query, data)
+	result = cursor.fetchall()
+	cnx.close()
+	return render_template('customer/profile.html', data=result[0])
 
-@app.route('/customer/search')
+@app.route('/customer/search', methods=['GET'])
 def search():
-	return render_template('customer/search.html')
+	
+	genre = request.args.get('genre')
+	title = request.args.get('title')
+	seats = request.args.get('seats')
+	start = request.args.get('start')
+	end = request.args.get('end')
+
+	if genre is None: genre = ""
+	if title is None: title = ""
+	if start == "": start = None
+	if end == "": end = None
+	
+	if seats is None:
+		seats = 0
+	else: seats = 1
+	
+	genre = "%" + genre + "%"
+	title = "%" + title + "%"
+	print "START", start
+	print "END", end
+
+	# Get show data
+	cnx = mysql.connector.connect(**config)
+	cursor = cnx.cursor()
+	query = ("select distinct m.MovieName, s.ShowingDateTime, s.TicketPrice, s.idShowing, c.idCustomer from Showing s"
+			" join Movie m on s.Movie_idMovie=m.idMovie"
+			" join Genre g on g.Movie_idMovie=m.idMovie"
+			" join Customer c"
+			" where c.EmailAddress=%s and g.Genre like %s and m.MovieName like %s"
+			" and (s.ShowingDateTime >= %s or %s is null)"
+			" and (s.ShowingDateTime <= %s or %s is null)")
+	print "GENRE", genre, "TITLE", title, request.args.get('title')
+	
+	data=(session['username'], genre, title, start, start, end, end)
+	cursor.execute(query, data)
+	result = cursor.fetchall()
+	
+	# Get empty seats
+	for i in range(len(result)):
+		data = (result[i][3],)
+		query=("select Capacity  from Showing join TheatreRoom on TheatreRoom_RoomNumber=RoomNumber where idShowing=%s")
+		cursor.execute(query,data)
+		capacity = cursor.fetchall()[0][0]
+		query=("select count(*) from Showing"
+		" join TheatreRoom on TheatreRoom_RoomNumber=RoomNumber"
+		" join Attend on Showing_idShowing=idShowing"
+		" where idShowing=%s group by Capacity")
+		cursor.execute(query,data)
+		occupied = cursor.fetchall()
+		if (len(occupied) == 0):
+			occupied = 0
+		else:
+			occupied = occupied[0][0]
+			
+		remaining = capacity - occupied
+		result[i] = result[i] + (remaining,)
+		print result[i]
+	# get list of genres
+	query = ("select distinct Genre from Genre")
+	cursor.execute(query)
+	genre = cursor.fetchall()
+	# get date range
+	query = ("select distinct ShowingDateTime from Showing order by ShowingDateTime")
+	cursor.execute(query)
+	dates = cursor.fetchall()
+	cnx.close()
+	return render_template('customer/search.html', data=result, genre=genre, seats=seats, dates=dates)
+
+@app.route('/customer/search/buy', methods=['POST'])
+def search_buy():
+	return redirect(url_for('search'))
 
 @app.route('/customer/ratings')
 def ratings():
-	return render_template('customer/ratings.html')
+	cnx = mysql.connector.connect(**config)
+	cursor = cnx.cursor()
+	query = ("select MovieName, idShowing, ShowingDateTime, Rating, Customer_idCustomer from Attend"
+			" join Customer on Customer_idCustomer=idCustomer and EmailAddress=%s"
+			" join Showing on Showing_idShowing=idShowing"
+			" join Movie on Movie_idMovie=idMovie")
+	data = (session['username'],)
+	cursor.execute(query, data)
+	result = cursor.fetchall()
+	cnx.close()
+	return render_template('customer/ratings.html', data=result)
 # --
+
+@app.route('/customer/ratings/modify', methods=['POST'])
+def ratings_modify():
+	cnx = mysql.connector.connect(**config)
+	cursor = cnx.cursor()
+	query = ("update Attend set Rating=%s where Showing_idShowing=%s and Customer_idCustomer=%s")
+	data=(request.form['rating'], request.form['showingid'], request.form['customerid'])
+	cursor.execute(query, data)
+	cnx.commit()
+	cnx.close()
+	return redirect(url_for('ratings'))
 
 # Backend pages
 @app.route('/backend')
